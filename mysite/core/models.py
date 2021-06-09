b0a2bf373d6faa362e46d5ce93f3f7d54b258e04
JiYoungDo <carrie0129@naver.com>
2021-06-09 19:42:00 +0900

    settings

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ImamgeUpload(models.Model):
+    title = models.CharField(max_length=100)
+    pic = models.FileField(null=True, blank =True, upload_to="")
+
+    def __str__(self):
+        return self.title
