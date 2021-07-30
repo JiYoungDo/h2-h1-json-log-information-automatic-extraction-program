d88ddec2202ed52a9e275d93822d4b11b2db54b9
JiYoungDo <carrie0129@naver.com>
2021-07-30 18:47:17 +0900

    h2 value -1 modify

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -131,7 +131,7 @@ def multi_chart(request,group_name,host_name, datas_array_h1, datas_array_h2):
         
         for i in range(len_min):
                 categories_list.append( {'label': str(i+1)} )
-                h1_value.append( {'value' : str(datas_array_h1[i])} )
+                h1_value.append( {'value' : str(datas_array_h1[i]*(-1))} )
                 h2_value.append( {'value' : str(datas_array_h2[i]*(-1))} )
 
 
